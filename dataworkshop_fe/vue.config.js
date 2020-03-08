5d181e21203f37fd5bd8f59c4e77e11030702914
ramon <ramonmorell@yahoo.es>
2020-03-08 22:43:52 +0100

    (app): combine django and vue

--- dataworkshop_fe/vue.config.js
+++ dataworkshop_fe/vue.config.js
@@ -1,10 +1,19 @@
+const BundleTracker = require('webpack-bundle-tracker');
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'es',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+    pluginOptions: {
+        i18n: {
+            locale: 'es',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false
+        }
+    },
+    publicPath: 'static/',
+    configureWebpack: {
+        plugins: [
+            new BundleTracker({
+                filename: '../dataworkshop_fe/webpack-stats.json'
+            })
+        ]
     }
-  }
-}
+};
